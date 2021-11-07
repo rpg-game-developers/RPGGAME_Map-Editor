4d002bec3f4b0dc478ccf11235a813a6a367c26a
Ruben <rubeneekhof@gmail.com>
2021-11-07 12:00:32 +0100

    Creating individual views and gave them the proper layout

---
+++ src/main/java/com/rpggame/mapeditor/view/MapEditingPanel.java
@@ -0,0 +1,10 @@
+package com.rpggame.mapeditor.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MapEditingPanel extends JPanel {
+
+	public MapEditingPanel() {
+	}
+}
--- src/main/java/com/rpggame/mapeditor/view/MapEditorWindow.java
+++ src/main/java/com/rpggame/mapeditor/view/MapEditorWindow.java
@@ -1,6 +1,7 @@
 package com.rpggame.mapeditor.view;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class MapEditorWindow {
 
@@ -10,6 +11,18 @@ public class MapEditorWindow {
 
 	public void buildAndShowView() {
 		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setLocationRelativeTo(null);
+
+		JPanel root = new JPanel();
+		root.setLayout(new BorderLayout());
+		root.add(new TileSelectorView(), BorderLayout.EAST);
+		root.add(new MapEditingPanel(), BorderLayout.CENTER);
+		root.add(new LayerPanelView(), BorderLayout.WEST);
+
+		frame.add(root);
+		frame.setVisible(true);
 	}
 
 }
