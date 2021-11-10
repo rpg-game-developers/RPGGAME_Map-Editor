b1a6af5fc971c524e70b7ac51aeac1860419e007
Ruben <rubeneekhof@gmail.com>
2021-11-10 18:24:09 +0100

    Fixed Constants

--- src/main/java/com/rpggame/mapeditor/constants/MapEditorConstants.java
+++ src/main/java/com/rpggame/mapeditor/constants/MapEditorConstants.java
@@ -5,6 +5,11 @@ import java.awt.*;
 public interface MapEditorConstants {
 
 	public final static int TILE_SIZE = 17;
+	public static final Color PRIMARY = new Color(60, 63, 65);
+	public final static Color DARK_BACKGROUND = new Color(10, 10, 10);
+	public static final Color LIGHT_GRAY = new Color(169, 169, 169);
+	public static final Color DARK_GRAY = new Color(50, 50, 50);
+	public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
 	public final static Font HEADER_FONT = new Font("Comic Sans", Font.BOLD, 20);
 	public final static Font REGULAR_FONT = new Font("Comic Sans", Font.BOLD, 15);
 
