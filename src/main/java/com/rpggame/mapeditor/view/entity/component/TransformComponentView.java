afcbf27801f42b58e0f2ae66f0654acb26ac0fa2
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-11-21 23:18:07 +0100

    Changing the scale and rotation of an entity

--- src/main/java/com/rpggame/mapeditor/view/entity/component/TransformComponentView.java
+++ src/main/java/com/rpggame/mapeditor/view/entity/component/TransformComponentView.java
@@ -1,28 +1,65 @@
 package com.rpggame.mapeditor.view.entity.component;
 
+import com.badlogic.gdx.math.Vector2;
 import com.rpggame.rpggame.component.physics.TransformComp;
 import imgui.ImGui;
 import imgui.flag.ImGuiDataType;
 import imgui.type.ImFloat;
 
+import javax.xml.crypto.dsig.Transform;
+
 public class TransformComponentView extends ComponentView<TransformComp> {
-    private ImFloat x;
-    private ImFloat y;
+    private final ImFloat x;
+    private final ImFloat y;
+    private final ImFloat xScale;
+    private final ImFloat yScale;
+    private final ImFloat rotation;
 
     public TransformComponentView() {
         super(TransformComp.class, TransformComp::new, "Transform");
         this.x = new ImFloat();
         this.y = new ImFloat();
+        this.xScale = new ImFloat();
+        this.yScale = new ImFloat();
+        this.rotation = new ImFloat();
     }
 
     @Override
     public void imGui() {
-        x.set(comp.getX());
-        y.set(comp.getY());
+        Vector2 position = comp.getPosition();
+        Vector2 scale = comp.getScale();
+        float rotationRad = comp.getRotation();
+
+        x.set(position.x);
+        y.set(position.y);
+        ImGui.pushID(0);
         ImGui.text("Position");
         ImGui.inputScalar("X", ImGuiDataType.Float, x);
         ImGui.inputScalar("Y", ImGuiDataType.Float, y);
-        comp.setX(x.get());
-        comp.setY(y.get());
+        ImGui.popID();
+
+        xScale.set(scale.x);
+        yScale.set(scale.y);
+        ImGui.pushID(1);
+        ImGui.text("Scale");
+        ImGui.inputScalar("X", ImGuiDataType.Float, xScale);
+        ImGui.inputScalar("Y", ImGuiDataType.Float, yScale);
+        ImGui.popID();
+
+        rotation.set(rotationRad);
+        ImGui.pushID(2);
+        ImGui.text("Rotation");
+        ImGui.inputScalar("Radians", ImGuiDataType.Float, rotation);
+        ImGui.popID();
+
+        if (position.x != x.get() || position.y != y.get()) {
+            comp.setPosition(new Vector2(x.get(), y.get()));
+        }
+        if (scale.x != xScale.get() || scale.y != yScale.get()) {
+            comp.setScale(new Vector2(xScale.get(), yScale.get()));
+        }
+        if (rotationRad != rotation.get()) {
+            comp.setRotation(rotation.get());
+        }
     }
 }
--- src/main/resources/scripts/test.js
+++ src/main/resources/scripts/test.js
@@ -1,8 +1,12 @@
 var TransformComp = Java.type("com.rpggame.rpggame.component.physics.TransformComp");
+var Vector2 = Java.type("com.badlogic.gdx.math.Vector2");
 
 function onRender(entity) {
     var trans = entity.getComponent(TransformComp.class);
-    var x = trans.getX();
+    var x = trans.getRotation();
+    trans.setRotation(x + 0.1);
+    trans.setScale(new Vector2(2, 1));
 }
+
 
 
